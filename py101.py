280c1a48094f63944de70c1fb90745eb371f75c3
change0z <103916203+change0z@users.noreply.github.com>
2023-02-05 16:24:41 +0000

    Sulagnas Python Journey File

---
+++ py101.py
@@ -0,0 +1,40 @@
+## To print any normal text, you use  --> print ("")
+# x is an integer
+#x = 5 + 5
+#print("The asnwer to the question is " + str(x))
+
+#y = "Ziyad"
+
+#rint("Author of this code is " + y)
+
+n = "boyfriend"
+#print(y + " is my " + n + " and he is a " + str(x))
+
+################################################################
+#name = "Kevin"
+
+#if name != "Kevin" :
+
+#    print("This is my " + n)
+#else:
+#    print("This is not my " + n)
+
+
+################################################################
+
+new_name = input("Enter your name: ")
+
+Age = input("Enter your age: ")
+Age = int(Age)
+
+
+if new_name == "Ziyad Shaikh":
+    print(" Hello " + new_name)
+else:
+    print("invalid")
+
+if new_name == "Ziyad Shaikh" and Age == 24:
+    print("valid")
+else:
+    print("invalid")
+
