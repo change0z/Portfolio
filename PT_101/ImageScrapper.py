82b26496571f05954e965ecb61dee0dfe7552464
change0z <shaikh.ziyad.mr@gmail.com>
2023-07-17 22:06:33 +0100

    Minor update to the image scrapper and script of image resizing

--- PT_101/ImageScrapper.py
+++ PT_101/ImageScrapper.py
@@ -6,10 +6,13 @@ gis = GoogleImagesSearch('AIzaSyDCdpESdJy3FOeTWB1zngS153DqFRumw0w', '62af1a6d94d
 # Define search parameters
 search_params = {
     'q': 'room interiors',
-    'num': 10,
+    'num': 10000,
     'imgSize': 'large',
     'fileType': 'jpg',
-    'safe': 'high'
+    'safe': 'high',
+    'imgType': 'photo',
+    'rights': 'cc_publicdomain|cc_attribute|cc_sharealike|cc_noncommercial|cc_nonderived',
+    'siteSearch': 'https://www.zillow.com/ OR https://www.realtor.com/'
 }
 
 # Search for images
